43bae89f906438cb6e62eaed9b0c1e17f921d655
csnedecor <csnedecor@worthwild.com>
2015-10-01 23:47:19 -0400

    Makes a better mobile friendly menu

--- header.php
+++ header.php
@@ -23,42 +23,46 @@
 
   <div class="nav-bar">
     <header id="masthead" class="site-header" role="banner">
-      <div class="site-branding">
+      <div class="grid grid-pad">
+        <div class="site-branding">
 
-            <?php if ( get_theme_mod( 'readit_logo' ) ) : ?>
+              <?php if ( get_theme_mod( 'readit_logo' ) ) : ?>
 
-            <div class="site-logo">
+              <div class="site-logo">
 
-                <a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><img src='<?php echo esc_url( get_theme_mod( 'readit_logo' ) ); ?>' <?php if ( get_theme_mod( 'logo_size' ) ) : ?>width="<?php echo esc_attr( get_theme_mod( 'logo_size' ), __( '120', 'readit' )); ?>"<?php endif; ?> alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'>
-                      </a>
+                  <a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><img src='<?php echo esc_url( get_theme_mod( 'readit_logo' ) ); ?>' <?php if ( get_theme_mod( 'logo_size' ) ) : ?>width="<?php echo esc_attr( get_theme_mod( 'logo_size' ), __( '120', 'readit' )); ?>"<?php endif; ?> alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'>
+                        </a>
 
-            </div><!-- site-logo -->
+              </div><!-- site-logo -->
 
-        <?php else : ?>
+          <?php else : ?>
 
-            <hgroup>
-                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-            </hgroup>
+              <hgroup>
+                  <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+              </hgroup>
 
-        <?php endif; ?>
+          <?php endif; ?>
 
-      </div><!-- .site-branding -->
-    </header><!-- #masthead -->
+        </div><!-- .site-branding -->
+        <ul class="perrio-menu">
+          <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+        </ul>
+      </header><!-- #masthead -->
 
+        <div class="perrio-mobile-view navigation-container">
 
-      <div class="navigation-container">
+              <a class="cd-primary-nav-trigger" href="#0">
+                    <span class="cd-menu-icon"></span>
+                </a> <!-- cd-primary-nav-trigger -->
 
-            <a class="cd-primary-nav-trigger" href="#0">
-                  <span class="cd-menu-icon"></span>
-              </a> <!-- cd-primary-nav-trigger -->
-
-          </div>
+            </div>
 
       <nav>
-      <ul class="cd-primary-nav">
-        <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-      </ul>
-    </nav>
+        <ul class="perrio-mobile-view cd-primary-nav">
+          <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+        </ul>
+      </nav>
+    </div>
   </div>
 
   <div id="content" class="site-content">
